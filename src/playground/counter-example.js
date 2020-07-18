a23bda956330e9a1c953a678d00605e677ac4611
Brian <blee11@uw.edu>
2020-07-17 22:13:01 -0700

    Set up default props

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,7 +5,7 @@ class Counter extends React.Component {
 		this.handleMinusOne = this.handleMinusOne.bind(this);
 		this.handleReset = this.handleReset.bind(this);
 		this.state = {
-			count: 0
+			count: props.count
 		};
 	}
 
@@ -43,7 +43,11 @@ class Counter extends React.Component {
 	}
 }
 
-ReactDOM.render(<Counter />, document.getElementById('app'));
+Counter.defaultProps = {
+	count: 0
+};
+
+ReactDOM.render(<Counter count={1}/>, document.getElementById('app'));
 
 // let count = 0;
 // const addOne = () => {
